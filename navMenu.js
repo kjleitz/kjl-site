29712ffb1bd59cf8554b1217580c0555c74a0ba6
Keegan Leitz <kjleitz@gmail.com>
2016-09-13 17:21:03 +0000

    added practically useless compatibility + fixed logic for nav menu collapsing

--- navMenu.js
+++ navMenu.js
@@ -2,11 +2,25 @@ var burgerIcon = document.getElementById("burger_icon");
 
 burgerIcon.onclick = function(){
     toggleClassById("top_nav", "collapsed");
-};
+}
 
 window.addEventListener("click", function(event){
-    if(!event.target.matches("#burger_icon")){
+    var ep = Element.prototype;
+    
+    if(ep.matches){
+        // pass
+    } else if(ep.webkitMatchesSelector){
+        ep.matches = ep.webkitMatchesSelector;
+    } else if(ep.mozMatchesSelector){
+        ep.matches = ep.mozMatchesSelector;
+    } else if(ep.msMatchesSelector){
+        ep.matches = ep.msMatchesSelector;
+    } else if(ep.oMatchesSelector){
+        ep.matches = ep.oMatchesSelector;
+    }
+    
+    if(!(event.target.matches("#burger_icon") ||
+         event.target.matches("#top_nav a"))){
         remClassById("top_nav", "collapsed");
     }
 });
-
